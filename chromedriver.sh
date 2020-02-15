9743239215d1d3df9e2f9afa5567336e3777a5b5
Mark Hobson <markhobson@gmail.com>
2020-02-15 12:04:58 +0000

    Allow ChromeDriver to be reinstalled

--- chromedriver.sh
+++ chromedriver.sh
@@ -5,6 +5,6 @@ VERSION=80.0.3987.106
 wget -O /tmp/chromedriver.zip https://chromedriver.storage.googleapis.com/$VERSION/chromedriver_linux64.zip
 unzip /tmp/chromedriver.zip -d /tmp
 rm /tmp/chromedriver.zip
-mkdir /opt/chromedriver
+mkdir -p /opt/chromedriver
 mv /tmp/chromedriver /opt/chromedriver
-ln -s /opt/chromedriver/chromedriver /usr/bin/chromedriver
+ln -sf /opt/chromedriver/chromedriver /usr/bin/chromedriver
