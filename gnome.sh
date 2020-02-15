8b4c388b4bbcf4db2e9a5a6c305938d8e269516f
Mark Hobson <markhobson@gmail.com>
2020-02-15 19:06:51 +0000

    Disable fractional scaling as it's buggy :(

--- gnome.sh
+++ gnome.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Enable experimental fractional scaling
-gsettings set org.gnome.mutter experimental-features "['x11-randr-fractional-scaling']"
-
 # Remove icons from desktop
 gsettings set org.gnome.shell.extensions.desktop-icons show-home false
 gsettings set org.gnome.shell.extensions.desktop-icons show-trash false
