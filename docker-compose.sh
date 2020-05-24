48357b64637e6123fbd25dc06b1a52b4cf914c47
Mark Hobson <markhobson@gmail.com>
2020-05-24 11:46:25 +0100

    Install Docker Compose

---
+++ docker-compose.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Docker Compose
+
+VERSION=1.25.5
+
+curl -L "https://github.com/docker/compose/releases/download/$VERSION/docker-compose-$(uname -s)-$(uname -m)" \
+	-o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Bash auto-completion
+
+curl -L https://raw.githubusercontent.com/docker/compose/$VERSION/contrib/completion/bash/docker-compose \
+	-o /etc/bash_completion.d/docker-compose
