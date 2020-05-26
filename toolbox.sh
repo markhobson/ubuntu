2166c5b05f480868a15942341e4696ef4d62aa88
Mark Hobson <mark.hobson@blackpepper.com>
2020-05-26 22:16:57 +0100

    Upgrade Toolbox to 1.17

--- toolbox.sh
+++ toolbox.sh
@@ -2,7 +2,7 @@
 
 # JetBrains Toolbox
 
-VERSION=1.16.6319
+VERSION=1.17.6856
 
 wget -O /tmp/toolbox.tar.gz https://download.jetbrains.com/toolbox/jetbrains-toolbox-$VERSION.tar.gz
 tar zxvf /tmp/toolbox.tar.gz -C /tmp
