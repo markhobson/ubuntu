f347eb737edcdd25688f2868c002b55af140269d
Mark Hobson <mark.hobson@blackpepper.com>
2020-05-26 22:17:19 +0100

    Install .NET Core

---
+++ dotnet.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# .NET Core
+
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O /tmp/packages-microsoft-prod.deb
+dpkg -i /tmp/packages-microsoft-prod.deb
+
+apt update
+apt install -y dotnet-sdk-3.1
