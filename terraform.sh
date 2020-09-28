74db31a1cb1fc80b5db7bba6193c4ef1eb1c6ed0
Mark Hobson <mark.hobson@blackpepper.co.uk>
2020-09-28 16:00:55 +0100

    Install Terraform

---
+++ terraform.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Terraform
+
+VERSION=0.13.3
+
+curl https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_linux_amd64.zip -o /tmp/terraform.zip
+unzip -f /tmp/terraform.zip -d /usr/local/bin
+rm /tmp/terraform.zip
