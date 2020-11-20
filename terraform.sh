06bb2289596d942aab55008f06ccd1e2daeb6763
Mark Hobson <mark.hobson@blackpepper.co.uk>
2020-11-20 11:25:19 +0000

    Upgrade Terraform to 0.13.5

--- terraform.sh
+++ terraform.sh
@@ -2,7 +2,7 @@
 
 # Terraform
 
-VERSION=0.13.3
+VERSION=0.13.5
 
 curl https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_linux_amd64.zip -o /tmp/terraform.zip
 unzip -f /tmp/terraform.zip -d /usr/local/bin
