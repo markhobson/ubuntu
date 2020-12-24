50ae2a4d2acdd192afe0092a4a67663aa8f9e9d4
Mark Hobson <mark.hobson@blackpepper.co.uk>
2020-12-24 16:24:58 +0000

    Update Atom installation script

--- atom.sh
+++ atom.sh
@@ -2,7 +2,7 @@
 
 # Atom
 
-curl -L https://packagecloud.io/AtomEditor/atom/gpgkey | apt-key add -
+wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | apt-key add -
 sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 
 apt update
