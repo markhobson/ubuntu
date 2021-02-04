59545cd9c2316ffe300febbb86d2949f0d3fd1df
Mark Hobson <mark.hobson@blackpepper.co.uk>
2021-02-04 10:38:06 +0000

    Update Terraform to 0.14.5

--- terraform.sh
+++ terraform.sh
@@ -2,7 +2,7 @@
 
 # Terraform
 
-VERSION=0.13.5
+VERSION=0.14.5
 
 curl https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_linux_amd64.zip -o /tmp/terraform.zip
 unzip -f /tmp/terraform.zip -d /usr/local/bin
