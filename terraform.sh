507170638c9acad29cab87b6ac9ff705f53b9dc2
Mark Hobson <mark.hobson@blackpepper.co.uk>
2021-02-04 10:41:47 +0000

    Suppress unzip prompt to overwrite files

--- terraform.sh
+++ terraform.sh
@@ -5,5 +5,5 @@
 VERSION=0.14.5
 
 curl https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_linux_amd64.zip -o /tmp/terraform.zip
-unzip -f /tmp/terraform.zip -d /usr/local/bin
+unzip -o /tmp/terraform.zip -d /usr/local/bin
 rm /tmp/terraform.zip
