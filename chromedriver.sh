f33584e4707f35a2e9fa4f5c1f81c3cbb03d64ca
Mark Hobson <markhobson@gmail.com>
2021-07-25 08:56:15 +0100

    Upgrade ChromeDriver to 92

--- chromedriver.sh
+++ chromedriver.sh
@@ -2,7 +2,7 @@
 
 # ChromeDriver
 
-VERSION=91.0.4472.101
+VERSION=92.0.4515.43
 
 wget -O /tmp/chromedriver.zip https://chromedriver.storage.googleapis.com/$VERSION/chromedriver_linux64.zip
 unzip /tmp/chromedriver.zip -d /tmp
