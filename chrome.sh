0ea5b730546c5b7d2b0936f0316072ed8f562338
Mark Hobson <markhobson@gmail.com>
2021-08-16 16:00:57 +0100

    Add explicit sudo for Chrome

--- chrome.sh
+++ chrome.sh
@@ -3,5 +3,5 @@
 # Chrome
 
 wget -O /tmp/chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i /tmp/chrome.deb
+sudo dpkg -i /tmp/chrome.deb
 rm /tmp/chrome.deb
