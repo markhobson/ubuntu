c80e011306ac85c8e44de793ba6e9e0d9e16b148
Mark Hobson <markhobson@gmail.com>
2021-08-16 16:28:08 +0100

    Add explicit sudo for Terraform

--- terraform.sh
+++ terraform.sh
@@ -3,5 +3,5 @@
 # Terraform
 
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-apt install terraform
+sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt install terraform
