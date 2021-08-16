e50b0152ec299e5b3a192b50d635eb2af2fc478d
Mark Hobson <markhobson@gmail.com>
2021-08-16 16:28:35 +0100

    Use stable apt command for Terraform

--- terraform.sh
+++ terraform.sh
@@ -4,4 +4,4 @@
 
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt install terraform
+sudo apt-get install terraform
