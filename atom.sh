2d58deb8497108ee13f353b4c70c337848b2361a
Mark Hobson <markhobson@gmail.com>
2021-08-16 16:14:59 +0100

    Use stable apt commands for Atom

--- atom.sh
+++ atom.sh
@@ -5,5 +5,5 @@
 wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 
-sudo apt update
-sudo apt install -y atom
+sudo apt-get update
+sudo apt-get install -y atom
