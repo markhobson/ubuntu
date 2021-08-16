9fb6ececb9f231df036ed29608a3ba44e6c38a4a
Mark Hobson <markhobson@gmail.com>
2021-08-16 16:04:18 +0100

    Install Node as per docs

--- node.sh
+++ node.sh
@@ -2,5 +2,5 @@
 
 # Node.js
 
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt install -y nodejs
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
