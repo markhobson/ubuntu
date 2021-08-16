ee0f931a056408a317c853199eb33d2d5c1a04cd
Mark Hobson <markhobson@gmail.com>
2021-08-16 16:30:18 +0100

    Install Terraform as per docs

--- terraform.sh
+++ terraform.sh
@@ -3,5 +3,7 @@
 # Terraform
 
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
+sudo apt-get update
 sudo apt-get install terraform
