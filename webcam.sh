0fcde03f708778f94eee8b5b8e51e271b5373eaa
Mark Hobson <mark.hobson@blackpepper.co.uk>
2021-08-17 12:09:02 +0100

    Add webcam script to adjust exposure

---
+++ webcam.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+	echo "webcam <device-number> <exposure-absolute>"
+	exit 0
+fi
+
+DEVICE=0
+if [ $# -eq 2 ]
+then
+	DEVICE=$1
+	shift
+fi
+
+VALUE=$1
+
+v4l2-ctl -d $DEVICE --set-ctrl exposure_auto=1
+v4l2-ctl -d $DEVICE --set-ctrl exposure_absolute=$VALUE
