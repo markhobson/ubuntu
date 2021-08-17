a1d4523192cae634fed3a100a3c3c56bdde0ad91
Mark Hobson <markhobson@gmail.com>
2021-08-17 18:19:29 +0100

    Add explicit sudo for Toolbox

--- toolbox.sh
+++ toolbox.sh
@@ -7,4 +7,4 @@ VERSION=1.21.9712
 wget -O /tmp/toolbox.tar.gz https://download.jetbrains.com/toolbox/jetbrains-toolbox-$VERSION.tar.gz
 tar zxvf /tmp/toolbox.tar.gz -C /tmp
 rm /tmp/toolbox.tar.gz
-mv /tmp/jetbrains-toolbox-$VERSION /opt
+sudo mv /tmp/jetbrains-toolbox-$VERSION /opt
