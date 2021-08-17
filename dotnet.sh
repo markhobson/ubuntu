a6ff8173b613154a6bbbcad9d76705051eb4621e
Mark Hobson <markhobson@gmail.com>
2021-08-17 18:18:13 +0100

    Remove dotnet package after install

--- dotnet.sh
+++ dotnet.sh
@@ -4,6 +4,7 @@
 
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O /tmp/packages-microsoft-prod.deb
 sudo dpkg -i /tmp/packages-microsoft-prod.deb
+rm /tmp/packages-microsoft-prod.deb
 
 sudo apt-get update
 sudo apt-get install -y dotnet-sdk-5.0
