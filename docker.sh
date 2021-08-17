0e2c98661dc78993311f1d9bd4f381fc607f3d65
Mark Hobson <markhobson@gmail.com>
2021-08-17 11:33:48 +0100

    Install Docker as per latest docs

--- docker.sh
+++ docker.sh
@@ -2,16 +2,16 @@
 
 # Docker
 
-DIST=$(lsb_release -cs)
 DOCKER_USER=mark
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $DIST stable"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+	| sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-apt update
-apt install -y docker-ce
+sudo apt-get update
+sudo apt-get install -y docker-ce
 
 # Manage Docker as a non-root user
 
-groupadd docker
-usermod -aG docker $DOCKER_USER
+sudo groupadd docker
+sudo usermod -aG docker $DOCKER_USER
