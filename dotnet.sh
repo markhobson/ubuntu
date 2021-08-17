4409e921f49ffc31c29ef0fcc9682e67169cd7e9
Mark Hobson <markhobson@gmail.com>
2021-08-17 18:10:31 +0100

    Use stable apt commands for dotnet

--- dotnet.sh
+++ dotnet.sh
@@ -5,5 +5,5 @@
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O /tmp/packages-microsoft-prod.deb
 sudo dpkg -i /tmp/packages-microsoft-prod.deb
 
-sudo apt update
-sudo apt install -y dotnet-sdk-5.0
+sudo apt-get update
+sudo apt-get install -y dotnet-sdk-5.0
