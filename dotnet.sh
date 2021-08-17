f9e7f2bdaf5b19133bcb240cb89eb4cb15f4febf
Mark Hobson <markhobson@gmail.com>
2021-08-17 18:10:00 +0100

    Add explicit sudo for dotnet

--- dotnet.sh
+++ dotnet.sh
@@ -3,7 +3,7 @@
 # .NET Core
 
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O /tmp/packages-microsoft-prod.deb
-dpkg -i /tmp/packages-microsoft-prod.deb
+sudo dpkg -i /tmp/packages-microsoft-prod.deb
 
-apt update
-apt install -y dotnet-sdk-5.0
+sudo apt update
+sudo apt install -y dotnet-sdk-5.0
