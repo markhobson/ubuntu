4ab084b6f910e6cd5d8e7bebae9d18dbc9b669bb
Mark Hobson <markhobson@gmail.com>
2021-09-04 20:26:45 +0100

    Add explicit sudo for Maven

--- maven.sh
+++ maven.sh
@@ -8,13 +8,13 @@ VERSION=3.8.2
 wget -O /tmp/maven.tar.gz $MIRROR/apache/maven/maven-3/$VERSION/binaries/apache-maven-$VERSION-bin.tar.gz
 tar zxvf /tmp/maven.tar.gz -C /tmp
 rm /tmp/maven.tar.gz
-mv /tmp/apache-maven-$VERSION /usr/local/
+sudo mv /tmp/apache-maven-$VERSION /usr/local/
 
-update-alternatives \
+sudo update-alternatives \
 	--install /usr/bin/mvn mvn /usr/local/apache-maven-$VERSION/bin/mvn 0 \
 	--slave /usr/bin/mvnDebug mvnDebug /usr/local/apache-maven-$VERSION/bin/mvnDebug
 
 # Bash auto-completion
 
-wget https://raw.github.com/dimaj/maven-bash-completion/master/bash_completion.bash \
+sudo wget https://raw.github.com/dimaj/maven-bash-completion/master/bash_completion.bash \
 	--output-document /etc/bash_completion.d/mvn
