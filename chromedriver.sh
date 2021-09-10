4d183b0a24f9f339373f04f1ae1937941c465679
Mark Hobson <markhobson@gmail.com>
2021-09-10 21:10:52 +0100

    Add explicit sudo for ChromeDriver

--- chromedriver.sh
+++ chromedriver.sh
@@ -7,6 +7,6 @@ VERSION=92.0.4515.43
 wget -O /tmp/chromedriver.zip https://chromedriver.storage.googleapis.com/$VERSION/chromedriver_linux64.zip
 unzip /tmp/chromedriver.zip -d /tmp
 rm /tmp/chromedriver.zip
-mkdir -p /opt/chromedriver
-mv /tmp/chromedriver /opt/chromedriver
-ln -sf /opt/chromedriver/chromedriver /usr/bin/chromedriver
+sudo mkdir -p /opt/chromedriver
+sudo mv /tmp/chromedriver /opt/chromedriver
+sudo ln -sf /opt/chromedriver/chromedriver /usr/bin/chromedriver
