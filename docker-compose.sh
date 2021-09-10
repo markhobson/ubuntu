de1286f11aee7795852b5d3461384be1d3cb9c7a
Mark Hobson <markhobson@gmail.com>
2021-09-10 21:47:20 +0100

    Upgrade Docker Compose to 1.29.2

--- docker-compose.sh
+++ docker-compose.sh
@@ -2,7 +2,7 @@
 
 # Docker Compose
 
-VERSION=1.29.0
+VERSION=1.29.2
 
 curl -L "https://github.com/docker/compose/releases/download/$VERSION/docker-compose-$(uname -s)-$(uname -m)" \
 	-o /usr/local/bin/docker-compose
