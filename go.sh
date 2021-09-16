200aacf2632075ecd5257aa7ad2858b93488f9a2
Mark Hobson <markhobson@gmail.com>
2021-09-16 12:00:22 +0100

    Upgrade Go to 1.17.1 by downloading directly

--- go.sh
+++ go.sh
@@ -2,5 +2,13 @@
 
 # Go
 
-sudo apt-get update
-sudo apt-get install -y golang-go
+VERSION=1.17.1
+
+wget -O /tmp/go.tar.gz https://golang.org/dl/go$VERSION.linux-amd64.tar.gz
+tar -C /tmp -xzf /tmp/go.tar.gz
+rm /tmp/go.tar.gz
+sudo mv /tmp/go /usr/local/go-$VERSION
+
+sudo update-alternatives \
+	--install /usr/bin/go go /usr/local/go-$VERSION/bin/go 0 \
+	--slave /usr/bin/gofmt gofmt /usr/local/go-$VERSION/bin/gofmt
