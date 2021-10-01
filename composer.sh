47bd833759abebe7217c7ec8c6ab8a328e3e8ab4
Mark Hobson <markhobson@gmail.com>
2021-10-01 09:28:35 +0100

    Install Composer

---
+++ composer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Composer
+
+sudo apt-get update
+sudo apt-get install -y php7.4-curl php7.4-xml
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+sudo mv composer.phar /usr/local/bin/composer
