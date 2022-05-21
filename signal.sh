e6dc51e1b8f35e32492147e73c8c576e1f1c2af2
Mark Hobson <markhobson@gmail.com>
2022-05-21 08:43:02 +0100

    Update Signal script

--- signal.sh
+++ signal.sh
@@ -2,8 +2,12 @@
 
 # Signal
 
-curl -s https://updates.signal.org/desktop/apt/keys.asc | apt-key add -
-echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | tee -a /etc/apt/sources.list.d/signal-xenial.list
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > /tmp/signal-desktop-keyring.gpg
+cat /tmp/signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+rm /tmp/signal-desktop-keyring.gpg
 
-apt update
-apt install -y signal-desktop
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' | \
+	sudo tee /etc/apt/sources.list.d/signal-xenial.list
+
+sudo apt update
+sudo apt install -y signal-desktop
