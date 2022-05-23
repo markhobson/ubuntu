5242fcfb141d6f6e93c3820c7a6456b5de0abb74
Mark Hobson <markhobson@gmail.com>
2022-05-23 12:48:04 +0100

    Upgrade Toolbox for Ubuntu 22.04

--- toolbox.sh
+++ toolbox.sh
@@ -4,6 +4,8 @@
 
 VERSION=1.24.11947
 
+sudo apt-get install libfuse2
+
 wget -O /tmp/toolbox.tar.gz https://download.jetbrains.com/toolbox/jetbrains-toolbox-$VERSION.tar.gz
 tar zxvf /tmp/toolbox.tar.gz -C /tmp
 rm /tmp/toolbox.tar.gz
