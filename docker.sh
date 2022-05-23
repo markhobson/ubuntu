14da59fd13c7a12538ee1ce70c9b85512577d50d
Mark Hobson <markhobson@gmail.com>
2022-05-23 14:29:10 +0100

    Update Docker script

--- docker.sh
+++ docker.sh
@@ -2,10 +2,8 @@
 
 # Docker
 
-DOCKER_USER=mark
-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
 	| sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
@@ -14,4 +12,4 @@ sudo apt-get install -y docker-ce
 # Manage Docker as a non-root user
 
 sudo groupadd docker
-sudo usermod -aG docker $DOCKER_USER
+sudo usermod -aG docker $USER
