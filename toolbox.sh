17206c90ad8596f62f29cf87011f8311fff4f313
Mark Hobson <markhobson@gmail.com>
2022-05-23 12:44:14 +0100

    Upgrade Toolbox to 1.24.11947

--- toolbox.sh
+++ toolbox.sh
@@ -2,7 +2,7 @@
 
 # JetBrains Toolbox
 
-VERSION=1.21.9712
+VERSION=1.24.11947
 
 wget -O /tmp/toolbox.tar.gz https://download.jetbrains.com/toolbox/jetbrains-toolbox-$VERSION.tar.gz
 tar zxvf /tmp/toolbox.tar.gz -C /tmp
