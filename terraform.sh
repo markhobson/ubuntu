9419962a95ea77f0afc5db1545c831b3c3d3dd06
Mark Hobson <markhobson@gmail.com>
2022-05-23 14:55:19 +0100

    Update Terraform for Ubuntu 22.04

--- terraform.sh
+++ terraform.sh
@@ -2,7 +2,7 @@
 
 # Terraform
 
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo tee /etc/apt/trusted.gpg.d/hashicorp.asc
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 
 sudo apt-get update
