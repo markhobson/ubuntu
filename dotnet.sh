8e1ff25f5af3eef93feaf56e777e66fe7e601675
Mark Hobson <markhobson@gmail.com>
2022-05-23 12:40:41 +0100

    Upgrade dotnet for Ubuntu 22.04

--- dotnet.sh
+++ dotnet.sh
@@ -2,7 +2,7 @@
 
 # .NET Core
 
-wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O /tmp/packages-microsoft-prod.deb
+wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O /tmp/packages-microsoft-prod.deb
 sudo dpkg -i /tmp/packages-microsoft-prod.deb
 rm /tmp/packages-microsoft-prod.deb
 
