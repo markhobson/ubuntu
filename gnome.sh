d06b1e32bd322016b10ca83a76eb0d0da3cbea6b
Mark Hobson <markhobson@gmail.com>
2022-05-24 10:21:49 +0100

    GNOME script redundant in Ubuntu 22.04

--- gnome.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Gnome settings
-
-# Remove icons from desktop
-
-gsettings set org.gnome.shell.extensions.desktop-icons show-home false
-gsettings set org.gnome.shell.extensions.desktop-icons show-trash false
