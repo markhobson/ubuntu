9146b6038590d6f7faeb630b3d9398224d6cf25e
Mark Hobson <markhobson@gmail.com>
2022-07-05 17:41:48 +0100

    Add explicit sudo for geckodriver

--- geckodriver.sh
+++ geckodriver.sh
@@ -7,6 +7,6 @@ VERSION=v0.31.0
 wget -O /tmp/geckodriver.tar.gz https://github.com/mozilla/geckodriver/releases/download/$VERSION/geckodriver-$VERSION-linux64.tar.gz
 tar xzf /tmp/geckodriver.tar.gz -C /tmp
 rm /tmp/geckodriver.tar.gz
-mkdir -p /opt/geckodriver
-mv /tmp/geckodriver /opt/geckodriver
-ln -sf /opt/geckodriver/geckodriver /usr/bin/geckodriver
+sudo mkdir -p /opt/geckodriver
+sudo mv /tmp/geckodriver /opt/geckodriver
+sudo ln -sf /opt/geckodriver/geckodriver /usr/bin/geckodriver
