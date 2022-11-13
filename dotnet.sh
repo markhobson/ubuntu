2f560e7d5bc4d19b18c5412559c0965522ba429a
Mark Hobson <markhobson@gmail.com>
2022-11-13 11:53:24 +0000

    Install .NET from Microsoft PPA

--- dotnet.sh
+++ dotnet.sh
@@ -8,5 +8,13 @@ wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.
 sudo dpkg -i /tmp/packages-microsoft-prod.deb
 rm /tmp/packages-microsoft-prod.deb
 
+# Install from Microsoft instead of Ubuntu
+# See: https://github.com/dotnet/core/issues/7699
+cat << EOF | sudo tee /etc/apt/preferences.d/microsoft-dotnet
+Package: *
+Pin: origin "packages.microsoft.com"
+Pin-Priority: 1001
+EOF
+
 sudo apt-get update
 sudo apt-get install -y dotnet-sdk-6.0
