b6eb3333aece2442973a34f3ce84f8cf57431e41
Mark Hobson <markhobson@gmail.com>
2022-11-13 10:47:40 +0000

    Update Docker GPG key location

--- docker.sh
+++ docker.sh
@@ -4,8 +4,8 @@ set -e
 
 # Docker
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
 	| sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
