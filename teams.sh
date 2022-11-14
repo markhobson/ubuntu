8cb002debf407a429d1d233ccada261306188e5f
Mark Hobson <markhobson@gmail.com>
2022-11-14 11:01:51 +0000

    Install Teams using latest instructions

--- teams.sh
+++ teams.sh
@@ -3,9 +3,11 @@
 set -e
 
 # Microsoft Teams
+# See: https://learn.microsoft.com/en-us/microsoftteams/get-clients?tabs=Linux#install-manually-from-the-command-line
 
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main" > /etc/apt/sources.list.d/teams.list'
+curl -s https://packages.microsoft.com/keys/microsoft.asc \
+	| sudo gpg --dearmor -o /usr/share/keyrings/microsoft-archive-keyring.gpg
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/ms-teams stable main" > /etc/apt/sources.list.d/teams.list'
 
 sudo apt-get update
 sudo apt-get install teams
