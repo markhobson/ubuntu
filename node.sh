8886cdfa386a2932af7a9e70454a07a734822195
Mark Hobson <markhobson@gmail.com>
2022-11-14 10:15:55 +0000

    Upgrade Node to 18

--- node.sh
+++ node.sh
@@ -4,5 +4,5 @@ set -e
 
 # Node.js
 
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
