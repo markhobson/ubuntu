232f848fb8476054d01193ab00e0b54980d8b5fd
Mark Hobson <markhobson@gmail.com>
2022-12-27 16:53:28 +0000

    Install Docker Compose via repository

--- docker.sh
+++ docker.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-# Docker
+# Docker & Docker Compose
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
 	| sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install -y docker-ce
+sudo apt-get install -y docker-ce docker-compose-plugin
 
 # Manage Docker as a non-root user
 
