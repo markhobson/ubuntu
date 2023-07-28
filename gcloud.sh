343123adfaef2eff0fbaacc49cc14e033997c68d
Mark Hobson <markhobson@gmail.com>
2023-07-28 10:27:23 +0100

    Install Google Cloud CLI via apt
    Snap doesn't allow additional components to be installed.

--- gcloud.sh
+++ gcloud.sh
@@ -2,4 +2,10 @@
 
 # Google Cloud CLI
 
-sudo snap install google-cloud-cli --classic
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" \
+	| sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg \
+	| sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+
+sudo apt-get update
+sudo apt-get install -y google-cloud-cli
