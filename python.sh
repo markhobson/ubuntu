84d04055b3267678932977b56bf801ae382ca836
Mark Hobson <markhobson@gmail.com>
2023-10-10 11:54:11 +0100

    Upgrade Python to 3.11

--- python.sh
+++ python.sh
@@ -4,9 +4,10 @@ set -e
 
 # Python
 
+sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get update
-sudo apt-get install -y python3
+sudo apt-get install -y python3.11
 
 # venv
 
-sudo apt-get install -y python3-venv
+sudo apt-get install -y python3.11-venv
