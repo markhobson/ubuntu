e635cbb2f5756bde607c059503dac33eff85283a
Mark Hobson <markhobson@gmail.com>
2023-12-21 10:57:40 +0000

    Install Bitwarden CLI as binary
    Avoids snap permission errors accessing local file system.

--- bw.sh
+++ bw.sh
@@ -4,4 +4,7 @@ set -e
 
 # Bitwarden CLI
 
-sudo snap install bw
+wget -O /tmp/bw.zip 'https://vault.bitwarden.com/download/?app=cli&platform=linux'
+unzip /tmp/bw.zip -d /tmp
+mv /tmp/bw ~/bin
+rm /tmp/bw.zip
