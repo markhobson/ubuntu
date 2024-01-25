934e74a43181d8343fb8504400efb08b30876dcc
Mark Hobson <markhobson@gmail.com>
2024-01-25 08:48:38 +0000

    Update Zoom download URL

--- zoom.sh
+++ zoom.sh
@@ -4,6 +4,6 @@ set -e
 
 # Zoom
 
-wget -O /tmp/zoom.deb https://us04web.zoom.us/client/latest/zoom_amd64.deb
+wget -O /tmp/zoom.deb https://zoom.us/client/latest/zoom_amd64.deb
 sudo apt-get install -y /tmp/zoom.deb
 rm /tmp/zoom.deb
