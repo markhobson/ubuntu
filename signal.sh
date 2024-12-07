a6df121420ec9d3b216d32404d305137375d0b7a
Mark Hobson <markhobson@gmail.com>
2024-12-07 21:28:10 +0000

    Update Signal installation steps

--- signal.sh
+++ signal.sh
@@ -3,13 +3,15 @@
 set -e
 
 # Signal
+#
+# See: https://signal.org/download/
 
-wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > /tmp/signal-desktop-keyring.gpg
-cat /tmp/signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+wget -qO- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > /tmp/signal-desktop-keyring.gpg
+cat /tmp/signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
 rm /tmp/signal-desktop-keyring.gpg
 
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' | \
-	sudo tee /etc/apt/sources.list.d/signal-xenial.list
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' \
+	| sudo tee /etc/apt/sources.list.d/signal-xenial.list
 
-sudo apt update
-sudo apt install -y signal-desktop
+sudo apt-get update
+sudo apt-get install -y signal-desktop
