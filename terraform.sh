8afa1bcbe643e7f7c205cc941f9cf52ddb5c73c5
Mark Hobson <markhobson@gmail.com>
2024-12-07 21:20:41 +0000

    Update Terraform installation steps

--- terraform.sh
+++ terraform.sh
@@ -3,12 +3,12 @@
 set -e
 
 # Terraform
+#
+# See: https://developer.hashicorp.com/terraform/install#linux
 
 wget -qO- https://apt.releases.hashicorp.com/gpg \
-	| gpg --dearmor \
-	| sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg \
-	> /dev/null
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" \
+	| sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" \
 	| sudo tee /etc/apt/sources.list.d/hashicorp.list
 
 sudo apt-get update
