cef775434d12b160c69a3e7a6e33be01a559efcf
Mark Hobson <markhobson@gmail.com>
2024-12-07 21:11:44 +0000

    Update Docker installation steps

--- docker.sh
+++ docker.sh
@@ -3,14 +3,20 @@
 set -e
 
 # Docker & Docker Compose
+#
+# See: https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" \
 	| sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-compose-plugin
 
 # Manage Docker as a non-root user
+#
+# See: https://docs.docker.com/engine/install/linux-postinstall/#manage-docker-as-a-non-root-user
 
 sudo usermod -aG docker $USER
