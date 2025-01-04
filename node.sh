57cd760ef68b63c9906b7b8be3afff05e53ef54c
Mark Hobson <markhobson@gmail.com>
2025-01-04 10:01:07 +0000

    Upgrade Node to 22

--- node.sh
+++ node.sh
@@ -3,14 +3,17 @@
 set -e
 
 # Node.js
+#
+# See: https://github.com/nodesource/distributions/wiki/Repository-Manual-Installation
+
+NODE_MAJOR=22
 
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl gnupg
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 
-NODE_MAJOR=20
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_${NODE_MAJOR}.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
 sudo apt-get update
-sudo apt-get install nodejs -y
+sudo apt-get install -y nodejs
