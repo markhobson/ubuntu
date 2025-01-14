27a571192963ae27b524c60e2440b2f7dd559ac6
Mark Hobson <markhobson@gmail.com>
2025-01-14 10:48:17 +0000

    Update gcloud installation steps

--- gcloud.sh
+++ gcloud.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# Google Cloud CLI
+set -e
 
+# Google Cloud CLI
+#
+# See: https://cloud.google.com/sdk/docs/install#deb
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg \
+	| sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" \
 	| sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg \
-	| sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 
 sudo apt-get update
 sudo apt-get install -y google-cloud-cli
