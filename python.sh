ace4e11696835f755aac6b4ee5afca78c7afb11d
Mark Hobson <markhobson@gmail.com>
2025-01-27 17:15:42 +0000

    Upgrade Python to 3.13

--- python.sh
+++ python.sh
@@ -6,8 +6,8 @@ set -e
 
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get update
-sudo apt-get install -y python3.12
+sudo apt-get install -y python3.13
 
 # venv
 
-sudo apt-get install -y python3.12-venv
+sudo apt-get install -y python3.13-venv
