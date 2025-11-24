990bf093d07d85d8bea38951de9845f503ae22d4
Mark Hobson <markhobson@gmail.com>
2025-11-24 10:57:27 +0000

    Cite AWS installation instructions

--- aws.sh
+++ aws.sh
@@ -3,6 +3,8 @@
 set -e
 
 # AWS CLI
+# 
+# See: https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
 
 curl https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o /tmp/awscliv2.zip
 unzip /tmp/awscliv2.zip -d /tmp
