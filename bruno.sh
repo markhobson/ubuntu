53d0c8dd67b9e12f5bd72bb5f2e2b7ba214ba121
Mark Hobson <markhobson@gmail.com>
2026-02-13 17:51:23 +0000

    Update Bruno installation

--- bruno.sh
+++ bruno.sh
@@ -3,11 +3,16 @@
 set -e
 
 # Bruno
+#
+# See: https://docs.usebruno.com/bruno-basics/download
 
 sudo mkdir -p /etc/apt/keyrings
-sudo gpg --list-keys
-sudo gpg --no-default-keyring --keyring /etc/apt/keyrings/bruno.gpg --keyserver keyserver.ubuntu.com --recv-keys 9FA6017ECABE0266
-echo "deb [signed-by=/etc/apt/keyrings/bruno.gpg] http://debian.usebruno.com/ bruno stable" \
+curl -fsSL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x9FA6017ECABE0266" \
+	| gpg --dearmor \
+	| sudo tee /etc/apt/keyrings/bruno.gpg \
+	> /dev/null
+sudo chmod 644 /etc/apt/keyrings/bruno.gpg
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/bruno.gpg] http://debian.usebruno.com/ bruno stable" \
 	| sudo tee /etc/apt/sources.list.d/bruno.list
  
 sudo apt-get update
